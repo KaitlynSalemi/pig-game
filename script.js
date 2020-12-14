e19056af12972c17d935b3d20f19c4a8e4563766
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-12-14 15:24:45 -0600

    dice roll functionality

--- script.js
+++ script.js
@@ -2,8 +2,29 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEL.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${diceNumber}.png`;
+
+  if (diceNumber !== 1) {
+    currentScore += diceNumber;
+    current0El.textContent = currentScore; // change to current player later
+  } else {
+  }
+});
