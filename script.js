4456b453495d9975d846c6ff26ab9c4c7562faf6
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-12-15 15:14:32 -0600

    hold button functionality

--- script.js
+++ script.js
@@ -20,6 +20,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 btnRoll.addEventListener('click', function () {
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
 
@@ -30,10 +38,12 @@ btnRoll.addEventListener('click', function () {
     currentScore += diceNumber;
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+  switchPlayer();
 });
